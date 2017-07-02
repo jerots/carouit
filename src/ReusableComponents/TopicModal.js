02fb699efbb39dbcdc0d644172943ca8a8d648fb
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2017-07-03 03:19:43 +0800

    - input field in focus automatically after modal opens - 'Enter' key can submit form

--- src/ReusableComponents/TopicModal.js
+++ src/ReusableComponents/TopicModal.js
@@ -5,23 +5,28 @@ import {Modal, ModalManager, Effect} from 'react-dynamic-modal';
 
 class TopicModal extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {title: ''};
     }
 
+    componentDidMount(){
+        this.refs.input.focus();
+    }
+
 
     //change handler for input field. validates that length does not exceed 255
-    changeHandler(e){
+    changeHandler(e) {
         let value = e.target.value;
-        if (value.length < 255){
+        if (value.length < 255) {
             this.setState({title: value})
         }
     }
 
     //Call parent's pushNewTopic function to add to topic array in memory
-    submitNewTopic(){
-        if (this.state.title.trim().length === 0){
+    submitNewTopic(e) {
+        e.preventDefault();
+        if (this.state.title.trim().length === 0) {
             return;
         }
 
@@ -34,16 +39,20 @@ class TopicModal extends Component {
         return (
             <Modal
                 effect={Effect.RotateFromLeft3D}>
+                <form onSubmit={this.submitNewTopic.bind(this)}>
+                    <div className="modal-custom">
+                        <h1>New Topic</h1>
 
-                <div className="modal-custom">
-                    <h1>New Topic</h1>
-
-                    <input className="form-control" value={this.state.title} onChange={this.changeHandler.bind(this)} />
-                    <div className="modal-footer">
-                    <button className="btn btn-default" onClick={ModalManager.close}>Close</button>
-                    <button className="btn btn-danger" onClick={this.submitNewTopic.bind(this)}>Submit</button>
+                        <input className="form-control" ref="input" value={this.state.title}
+                               onChange={this.changeHandler.bind(this)}/>
+                        <div className="modal-footer">
+                            <button className="btn btn-default" onClick={ModalManager.close}>Close</button>
+                            <button className="btn btn-danger" type="submit">
+                                Submit
+                            </button>
+                        </div>
                     </div>
-                </div>
+                </form>
             </Modal>
         );
     }
