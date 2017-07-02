1f87752af74ad65ed5be835bd78fb83add81c580
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2017-07-02 23:55:14 +0800

    - Banner created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
+
+    constructor(props){
+        super()
+
+
+
+    }
+
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+          <div className="row banner">
+              <div className="col-xs-12">
+                  <span style={{color:'#c72121'}}>carou</span>it <span className="glyphicon glyphicon-comment" style={{color:'#c72121'}}/>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-xs-12">
+                  CONTENT HERE
+              </div>
+          </div>
       </div>
     );
   }
